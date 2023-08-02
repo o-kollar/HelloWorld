8f3483dec2a45a036c36e7f4aa944773aeaf53dd
Ondrej Kollar <o.kollarsocialmedia@gmail.com>
2023-08-02 20:15:24 +0200

    Cors Changes

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const routes = require('./src/router');
 
 let url;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // Serve static files from the "public" directory
 app.use(express.json());
 app.use(express.static('public'));
