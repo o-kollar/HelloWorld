bc5d879171d01fafc52d93631179bc8d5f249f9e
Ondrej Kollar <o.kollarsocialmedia@gmail.com>
2023-08-17 14:38:29 +0200

    Allow Headers cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers','*');
   next();
 });
 // Serve static files from the "public" directory
