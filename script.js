d31e531c9590ebe161c79bebd1a77fd6bf8a4015
Ondrej Kollar <o.kollarsocialmedia@gmail.com>
2023-08-26 02:47:11 +0200

    Adding option for natural language search.

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-maplibregl.setRTLTextPlugin('https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js');
 
 document.addEventListener("DOMContentLoaded", () => {
     const messageInput = document.getElementById("message-input");
-    const sendButton = document.getElementById("send-button");
-
-    sendButton.addEventListener("click", () => {
-        const message = messageInput.value.trim();
-        if (message !== "") {
-            // Create and send POST request here
-            sendMessage(message);
-            messageInput.value = ""; // Clear the input field
+    messageInput.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault(); // Prevent default Enter behavior (form submission)
+            sendMessage(messageInput.value.trim());
         }
     });
+    
 
     function sendMessage(message) {
         // Replace 'your-api-endpoint' with your actual API endpoint
-        fetch("http://localhost:3000/chat", {
+        fetch(`${url}/chat`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -30,7 +26,8 @@ document.addEventListener("DOMContentLoaded", () => {
             Data.speed = result.speed;
             Data.speeds = result.logs.speed;
             Data.path = result.logs.location;
-            Data.abc = result.parts;
+            Data.parts = result.parts;
+            console.log('PARTS DATA:',Data.parts)
             
             renderBar(result);
        renderChart(result.logs.updated, result.logs.altitude);
@@ -42,14 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-let mapstyle;
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        // Device is in dark mode
-       mapstyle =  './resources/dark.json'
-      } else {
-        // Device is not in dark mode
-        mapstyle =  './resources/default.json'
-      }
+
 
 const headers =  { 
     "ngrok-skip-browser-warning": 'true'
@@ -57,8 +47,9 @@ const headers =  {
 let url = "https://97c4-2a02-ab04-3d2-f800-a1a2-ddb-431c-b6e.ngrok-free.app"
   async function getDataAndRender(start, end) {
     let body = {type:'today'};
+
   
-    if (start) {
+    if (end) {
       body = {
         start: start,
         end: end,
@@ -73,6 +64,8 @@ let url = "https://97c4-2a02-ab04-3d2-f800-a1a2-ddb-431c-b6e.ngrok-free.app"
         },
         body: JSON.stringify(body)
       });
+
+      
   
       const result = await response.json();
   
@@ -84,7 +77,7 @@ let url = "https://97c4-2a02-ab04-3d2-f800-a1a2-ddb-431c-b6e.ngrok-free.app"
       Data.speed = result.speed;
       Data.speeds = result.logs.speed;
       Data.path = result.logs.location;
-      Data.abc = result.parts;
+      Data.parts = result.parts;
       if (!start) {
         await new Promise(resolve => setTimeout(resolve, 500));
       }
@@ -98,25 +91,12 @@ let url = "https://97c4-2a02-ab04-3d2-f800-a1a2-ddb-431c-b6e.ngrok-free.app"
   
   getDataAndRender()
 
-//_________
-fetch(`${url}/folder-contents`,{
-    headers: headers})
-    .then(response => response.json())
-    .then(data => {
-        Data.Route.routes = data.files
-        Data.totalSum = data.totalSum
-        let Topspeed = data.fastestSpeed * 3.6;
-        Data.fastestSpeed = Topspeed.toFixed(2)
-        Data.Route.selected = data.files[0]
-    })
-    .catch(error => {
-        console.error('Error fetching folder contents:', error);
-    });
 
 
 
 let Data = Alpine.reactive({
     query:'',
+    parts:[],
     Route:{selected:''}, 
     distance: '',
     duration: '',
@@ -163,34 +143,7 @@ let Data = Alpine.reactive({
     },
 });
 
-function fetchData(route) {
 
-    // Make a request to your server's API endpoint that serves the track.json data
-    fetch(`${url}/getRecord/${route}`,{
-        headers: headers}) // Replace "/api/track" with the actual API endpoint on your server
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            let coords = data.logs.location;
-            // new maplibregl.Marker({ color: '#FF5733' }).setLngLat(data.dates.today.data.location).addTo(map);
-            Data.distance = data.total;
-            Data.duration = data.duration;
-            Data.elevation = data.elevation;
-            Data.speed = data.speed;
-            Data.speeds = data.logs.speeds;
-            Data.path = data.logs.location;
-            Data.location = data.logs.location;
-            
-
-            renderChart(data);
-            renderBar(data);
-            console.log(Data.location)
-            loadMap()
-        })
-        .catch((error) => {
-            console.error('Error fetching data:', error);
-        });
-}
 
 
 
@@ -237,72 +190,11 @@ function renderChart(updated,altitude) {
 window.onload = function(){loadMap()}
 fetchData();
 
-function loadMap() {
-    
+function clearInput(){
+    Data.query = '';
+    getDataAndRender();
+}
 
-    const map = new maplibregl.Map({
-        container: 'map',
-        style: mapstyle,
-    });
-
-    map.on('load', () => {
-        const geojsonData = {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                        "type": "LineString",
-                        "coordinates": Data.path
-                    }
-                }
-            ]
-        };
-    
-        // save full coordinate list for later
-        const coordinates = geojsonData.features[0].geometry.coordinates;
-    
-        // start by showing just the first coordinate
-        geojsonData.features[0].geometry.coordinates = [coordinates[0]];
-    
-        // add it to the map
-        map.addSource('trace', {type: 'geojson', data: geojsonData});
-        map.addLayer({
-            'id': 'trace',
-            'type': 'line',
-            'source': 'trace',
-            'paint': {
-                'line-color': '#99f6e4',
-                'line-opacity': 0.75,
-                'line-width': 5
-            }
-        });
-    
-        // setup the viewport
-        map.jumpTo({'center': coordinates[0], 'zoom': 14});
-        // on a regular basis, add more coordinates from the saved list and update the map
-        let i = 0;
-        const timer = window.setInterval(() => {
-            if (i < coordinates.length) {
-                geojsonData.features[0].geometry.coordinates.push(
-                    coordinates[i]
-                );
-                map.getSource('trace').setData(geojsonData);
-                i++;
-                const bounds = coordinates.reduce((bounds, coord) => {
-                    return bounds.extend(coord);
-                }, new maplibregl.LngLatBounds(coordinates[0], coordinates[0]));
-        
-                map.fitBounds(bounds, {
-                    padding: 60
-                });                
-            } else {
-                window.clearInterval(timer);
-            }
-        }, 35);
-    });  
-};
 
 function renderBar(data){
 var ctx12 = document.getElementById("chart12").getContext("2d");
@@ -383,13 +275,4 @@ window.myBar = new Chart(ctx12, {
   }
 });}
 
-function findHighestSpeed(speeds) {
-    let highestSpeed = speeds[0]; // Assume the first speed is the highest initially
-  
-    for (let i = 1; i < speeds.length; i++) {
-      if (speeds[i] > highestSpeed) {
-        highestSpeed = speeds[i]; // Update the highest speed if a larger one is found
-      }
-    }
-    return highestSpeed.toFixed();
-  }
+
