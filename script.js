c063b1ae139066cce717336cc47804ebbdd319db
Ondrej Kollar <o.kollarsocialmedia@gmail.com>
2023-08-27 21:02:19 +0200

    Adding a favicon

--- script.js
+++ script.js
@@ -113,13 +113,87 @@ let Data = Alpine.reactive({
 });
 
 function updateData(result) {
+    activity = calculateTimeSpent(result.logs);
+
     Data.distance = result.total;
-    Data.duration = result.duration;
+    Data.duration = { total: result.duration,moving: formatTime(activity.movingTime),idle:formatTime(activity.inactiveTime)}
     Data.elevation = result.elevation;
     Data.speed = result.speed;
     Data.speeds = result.logs.speed;
     Data.path = result.logs.location;
-    Data.parts = result.parts;
+
+    // Temporarily on Frontend
+    Data.parts = separateLogsByTimeGap(result.logs.updated); 
+      
+    
+    console.log("Time spent moving:", formatTime(results.movingTime));
+    console.log("Time spent inactive:", formatTime(results.inactiveTime));
+    
+  
+    
+    function separateLogsByTimeGap(logsArray) {
+        const timeGapThreshold = 10 * 60 * 1000; // 10 minutes in milliseconds
+        const separatedLogs = [];
+        let currentPart = [];
+    
+        for (let i = 0; i < logsArray.length - 1; i++) {
+            const currentTime = new Date(logsArray[i]);
+            const nextTime = new Date(logsArray[i + 1]);
+    
+            if (nextTime - currentTime > timeGapThreshold) {
+                if (currentPart.length > 0) {
+                    separatedLogs.push({ start: currentPart[0], end: currentPart[currentPart.length - 1] });
+                }
+                currentPart = [];
+            }
+    
+            currentPart.push(logsArray[i]);
+        }
+    
+        if (currentPart.length > 0) {
+            separatedLogs.push({ start: currentPart[0], end: currentPart[currentPart.length - 1] });
+        }
+    
+        return separatedLogs;
+    }
+    
+   
+    
+    const separatedLogs = separateLogsByTimeGap(result.logs.updated);
+    console.log(separatedLogs);
+
+    function calculateTimeSpent(abc) {
+        const timestamps = abc.updated;
+        const speeds = abc.speed;
+    
+        let movingTime = 0;
+        let inactiveTime = 0;
+    
+        for (let i = 0; i < speeds.length; i++) {
+            const speed = parseFloat(speeds[i]);
+            
+            if (speed > 1) {  // Assuming that a speed greater than 1 indicates movement
+                if (i > 0) {
+                    const prevTimestamp = new Date(timestamps[i - 1]);
+                    const currentTimestamp = new Date(timestamps[i]);
+                    movingTime += currentTimestamp - prevTimestamp;
+                }
+            } else {
+                if (i > 0) {
+                    const prevTimestamp = new Date(timestamps[i - 1]);
+                    const currentTimestamp = new Date(timestamps[i]);
+                    inactiveTime += currentTimestamp - prevTimestamp;
+                }
+            }
+        }
+        return { movingTime, inactiveTime };
+    }
+    
+    function formatTime(milliseconds) {
+        const hours = Math.floor(milliseconds / 3600000);
+        const minutes = Math.floor((milliseconds % 3600000) / 60000);
+        return { hours: hours.toString(), minutes: minutes.toString() };
+    }  
 }
 
 
